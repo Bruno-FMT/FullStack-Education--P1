d21bf9dfc8cb74d8e1bee7b36f112a8e6467f7b2
CorreaBrunoMiguel <devsistemascorrea@gmail.com>
2024-02-15 08:30:27 -0300

    Encapsulamento Classe Aluno

--- objetos/Aluno.java
+++ objetos/Aluno.java
@@ -1,24 +1,27 @@
 package objetos;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Aluno {
+
     private String nome;
     private int idade;
-    public void listarCurso() {
+    private ArrayList<String> cursoMatriculado = new ArrayList<>();
 
+    public Aluno(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Aluno precisa ter um nome: ");
+        this.nome = input.nextLine();
     }
-    public void adicionarCurso() {
 
-    }
-    public void removerCurso() {
-
+    public Aluno(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
     }
 
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public int getIdade() {
@@ -26,6 +29,52 @@ public class Aluno {
     }
 
     public void setIdade(int idade) {
+        if (idade < 0 || idade > 120)
+            throw new IllegalArgumentException("Idade deve estar entre 0-120");
         this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void adicionarCurso(String curso){
+        this.cursoMatriculado.add(curso);
+    }
+
+    public void removerCurso() {
+        Scanner input = new Scanner(System.in);
+
+        listarCursos();
+        System.out.print("\nInforme o indice do curso para remover: ");
+        try {
+            cursoMatriculado.remove(Integer.parseInt(input.nextLine()) - 1);
+        } catch (IndexOutOfBoundsException erro){
+            System.out.println(erro.getMessage());
+        }
+    }
+
+    public void listarCursos(){
+
+        int contIndice = 1;
+
+        if (cursoMatriculado.isEmpty()){
+            System.out.println("Aluno não está matriculado em nenhum curso");
+        } else {
+            System.out.printf("| %2s | %14s |\n", "ID", "Nome do Curso");
+            for (String curso: cursoMatriculado){
+                System.out.printf("| %2d | %14s |\n",contIndice, curso);
+                contIndice++;
+            }
+        }
+    }
+    @Override
+    public String toString(){
+        System.out.printf("| %13s | %6s |\n", "Nome", "Idade");
+        return String.format(
+                "| %13s | %6d |",
+                this.getNome(),
+                this.getIdade()
+        );
     }
 }
