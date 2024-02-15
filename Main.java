9bfd3d73f37463e2fe88dc94b5dfaa4f91b07591
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-15 19:29:54 -0300

    cria tela solicitando nome para fazer login ou criar usuário

--- Main.java
+++ Main.java
@@ -7,16 +7,14 @@ public class Main {
 
         while (true) {
             imprimirMenu();
-            int opcaoSelecionada = Utils.nextInt(entrada);
-            switch (opcaoSelecionada) {
+            int itemSelecionadoMenu = Utils.nextInt(entrada);
+            switch (itemSelecionadoMenu) {
                 case 1:
                     String nomeFuncionario = login(entrada);
                     if (nomeFuncionario.equals("s")){
                         continue;
                     }
                     //TODO: adicionar login / menu de funcionários
-                    //Funcionario funcionario = new Funcionario();
-                    //funcionario.login()
                     break;
                 case 2:
                     String nomeAluno = login(entrada);
@@ -24,8 +22,6 @@ public class Main {
                         continue;
                     }
                     //TODO: adicionar login / menu de alunos
-                    //Aluno aluno = new Aluno();
-                    //aluno.login();
                     break;
                 case 0:
                     System.out.println("Finalizando programa....");
@@ -59,6 +55,10 @@ public class Main {
         System.out.println("\n████████████████████████████████████████████");
         System.out.println("                   LOGIN                   ");
         System.out.println("████████████████████████████████████████████");
+        System.out.println(
+                "Digite seu nome para fazer o login, " +
+                "caso não tenha cadastro iremos criar seu usuário."
+        );
         System.out.print("Informe seu nome ou [s] para sair: ");
         String nome = entrada.nextLine();
         return nome;
