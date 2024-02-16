112b211289cab70d06e857b48859204d160b52ba
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-16 00:19:36 -0300

    cria lista e métodos em DadosAlunos

--- dados/DadosAlunos.java
+++ dados/DadosAlunos.java
@@ -1,4 +1,34 @@
 package dados;
 
+import objetos.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DadosAlunos {
+    public static List<Aluno> alunosCadastrados = new ArrayList<>();
+
+    public void adicionarAluno(Aluno aluno) {
+        alunosCadastrados.add(aluno);
+        System.out.println("Aluno adicionado com sucesso.");
+    }
+
+    public void removerAlunoPorId(int id) {
+        for (Aluno aluno : alunosCadastrados) {
+            if (aluno.getId() == id) {
+                alunosCadastrados.remove(aluno);
+                System.out.println("Aluno removido com sucesso.");
+            }
+        }
+    }
+
+    public Aluno getAlunoPorId(int id) {
+        for (Aluno aluno : alunosCadastrados) {
+            if (aluno.getId() == id) {
+                return aluno;
+            }
+        }
+        System.out.println("Nenhum aluno encontrado com o ID informado.");
+        return null;
+    }
 }
