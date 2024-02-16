b35eab20e21e6d927ee8ee2619b5ed128e05b686
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-16 00:20:30 -0300

    cria DadosCursos e DadosTurmas

---
+++ dados/DadosCursos.java
@@ -0,0 +1,10 @@
+package dados;
+
+import objetos.Curso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DadosCursos {
+    public static List<Curso> cursosCadastrados = new ArrayList<>();
+}
---
+++ dados/DadosTurmas.java
@@ -0,0 +1,10 @@
+package dados;
+
+import objetos.Turma;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DadosTurmas {
+    public static List<Turma> tumasCadastradas = new ArrayList<>();
+}
