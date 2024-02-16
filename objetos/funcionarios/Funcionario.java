a2df9abf7c6156e707559c32d5fc843f4041ff22
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-16 00:18:14 -0300

    adiciona id em funcionario

--- objetos/funcionarios/Funcionario.java
+++ objetos/funcionarios/Funcionario.java
@@ -1,19 +1,41 @@
 package objetos.funcionarios;
 
 public class Funcionario {
+    private static int proximoId;
     private String nome;
     private int idade;
     private double salario;
     private int anosCargo;
+    private int id;
 
     public Funcionario(String nome, int idade, double salario, int anosCargo) {
         this.nome = nome;
         this.idade = idade;
         this.salario = salario;
         this.anosCargo = anosCargo;
+        this.id = proximoId;
+        proximoId++;
     }
 
-    public Funcionario() {}
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getAnosCargo() {
+        return anosCargo;
+    }
+
+    public void setAnosCargo(int anosCargo) {
+        this.anosCargo = anosCargo;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public String getNome() {
         return nome;
