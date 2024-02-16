5e101ec0258ea71b4bd447353ce1bdab599d950b
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-16 00:19:53 -0300

    cria lista e métodos em DadosDiretores

--- dados/DadosDiretores.java
+++ dados/DadosDiretores.java
@@ -1,4 +1,34 @@
 package dados;
 
+import objetos.funcionarios.Diretor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DadosDiretores {
+    public static List<Diretor> diretoresCadastrados = new ArrayList<>();
+
+    public void adicionarDiretor(Diretor diretor) {
+        diretoresCadastrados.add(diretor);
+        System.out.println("Aluno adicionado com sucesso.");
+    }
+
+    public void removerDiretorPorId(int id) {
+        for (Diretor diretor : diretoresCadastrados) {
+            if (diretor.getId() == id) {
+                diretoresCadastrados.remove(diretor);
+                System.out.println("Diretor removido com sucesso.");
+            }
+        }
+    }
+
+    public Diretor getDiretorPorId(int id) {
+        for (Diretor diretor : diretoresCadastrados) {
+            if (diretor.getId() == id) {
+                return diretor;
+            }
+        }
+        System.out.println("Nenhum Diretor encontrado com o ID informado.");
+        return null;
+    }
 }
