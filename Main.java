cdd4e9f074cae072007f7db2ad9ca2dabb4d5c44
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-23 15:39:24 -0300

    add métodos em professor

--- Main.java
+++ Main.java
@@ -16,23 +16,31 @@ public class Main {
         ArrayList<Aluno> alunos = new ArrayList<>();
         ArrayList<Aluno> alunos2 = new ArrayList<>();
         Aluno aluno = new Aluno("Jerry Smith", 69, "aluno", "aluno123");
+        Aluno aluno2 = new Aluno("Maria Silva", 30, "aluno2", "aluno123");
         alunos.add(aluno);
         alunos2.add(aluno);
+        alunos2.add(aluno2);
+
         ArrayList<Professor> professores = new ArrayList<>();
-        professores.add(new Professor("Gustavo", 30, 10000.00, 40));
+        Professor professor = new Professor("Gustavo", 30, 10000.00, 40);
+        professores.add(professor);
+
         Curso curso = new Curso("Java", professores);
         Curso curso2 = new Curso("HTML", professores);
+
         Turma java = new Turma(curso, alunos, 2025);
         Turma html = new Turma(curso2, alunos2, 2024);
-        aluno.listarCursosCadastrados();
-        aluno.sairCurso(curso2);
-        System.out.println();
-        aluno.listarCursosCadastrados();
-        System.out.println();
-        DadosTurmas.listarCursosCadastrados();
-        aluno.matricularCurso(curso2);
-        System.out.println();
-        aluno.listarCursosCadastrados();
+
+        System.out.println("professor.getCursos()");
+        System.out.println(professor.getCursos());
+        System.out.println("professor.getTurmas()");
+        System.out.println(professor.getTurmas());
+        System.out.println("professor.getTodosAlunos()");
+        System.out.println(professor.getTodosAlunos());
+        System.out.println("professor.listarTodosAlunos()");
+        professor.listarTodosAlunos();
+        System.out.println("professor.listarAlunoPorCurso()");
+        professor.listarAlunoPorCurso();
 
         System.out.println("******FIM*******");
     }
